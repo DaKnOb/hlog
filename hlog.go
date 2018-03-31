310811d7484b5de383fb2298f5b1fb371db1ed5d
DaKnOb <daknob.mac@gmail.com>
2018-03-31 17:01:04 +0300

    Add main code

---
+++ hlog.go
@@ -0,0 +1,22 @@
+package hlog
+
+import (
+	"net/http"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/google/uuid"
+)
+
+/*
+LogRequest will log an HTTP Request (r) to logrus and return its UUID
+*/
+func LogRequest(r *http.Request) string {
+	/* Generate a Request ID */
+	rid := uuid.New().String()
+
+	/* Log the Request */
+	logrus.Printf("%s: %s (%s) \"%s %s %s\" \"%s\" \"%s\"", rid, r.RemoteAddr, r.Header.Get("X-Forwarded-For"), r.Method, r.RequestURI, r.Proto, r.Referer(), r.Host)
+
+	/* Return the Request ID, for further logging */
+	return rid
+}
